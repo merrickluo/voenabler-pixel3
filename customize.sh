acfa370e6baeb97b966c3ece012e6521cbd547c1
A.I <merrick@luois.me>
2021-08-25 17:16:36 +0800

    modified for pixel 3 w/ China Unicom

--- customize.sh
+++ customize.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
 
-  ui_print "************************************"
-  ui_print " Enabling VoLTE/VoWiFi/RCS settings "
-  ui_print "************************************"
+ui_print "***************************************************************"
+ui_print " Enabling VoLTE/VoWiFi/RCS settings for Pixel 3 w/ China Unicom"
+ui_print "***************************************************************"
